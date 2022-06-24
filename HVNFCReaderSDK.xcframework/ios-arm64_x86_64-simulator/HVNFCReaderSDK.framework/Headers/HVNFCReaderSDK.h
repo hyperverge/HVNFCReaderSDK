38771e969ad197101c74cfd80191d31f912dca35
nivedita <nivedita@hyperverge.co>
2022-06-24 07:28:57 +0530

    Initial versionD

---
+++ HVNFCReaderSDK.xcframework/ios-arm64_x86_64-simulator/HVNFCReaderSDK.framework/Headers/HVNFCReaderSDK.h
@@ -0,0 +1,18 @@
+//
+//  HVNFCReaderSDK.h
+//  HVNFCReaderSDK
+//
+//  Created by Nivedita Muthusubramanian on 24/05/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HVNFCReaderSDK.
+FOUNDATION_EXPORT double HVNFCReaderSDKVersionNumber;
+
+//! Project version string for HVNFCReaderSDK.
+FOUNDATION_EXPORT const unsigned char HVNFCReaderSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HVNFCReaderSDK/PublicHeader.h>
+
+
